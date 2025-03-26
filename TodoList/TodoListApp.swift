a9c120c7000e274239527da9d38a517c74f4dbbd
Kevin Kwan <kevinkwan@Kevins-MacBook-Pro.local>
2025-03-25 23:47:38 -0400

    init

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Kevin Kwan on 2025-03-25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
