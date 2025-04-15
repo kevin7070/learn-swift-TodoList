fba83f9e10a4c141817a47ec3188e50000fc197d
Kevin Kwan <kevinkwan@Kevins-MacBook-Pro.local>
2025-04-14 23:41:06 -0400

    markdown text displaying

--- PracticeHub/ContentView.swift
+++ PracticeHub/ContentView.swift
@@ -24,6 +24,7 @@ struct ContentView: View {
                                 Color(.systemYellow))
                         Text("Todo List")
                             .foregroundColor(Color(.white))
+                            .shadow(color: .white, radius: 2, x: 0, y: 1)
                     }
                     .padding()
                     .background(Color(.black))
@@ -35,7 +36,7 @@ struct ContentView: View {
                 Spacer()
 
                 Text(
-                    "Practice Hub is a centralized platform designed to support focused learning, skill development, and performance tracking. Whether you’re a student, educator, or professional, Practice Hub offers a structured environment where users can engage with practice exercises, monitor progress, and achieve measurable growth over time. With customizable modules, intuitive dashboards, and real-time feedback, Practice Hub empowers users to build mastery and confidence in any area of study or training."
+                    "**Practice Hub** is a centralized platform designed to support focused learning, skill development, and performance tracking. Whether you’re a student, educator, or professional, Practice Hub offers a structured environment where users can engage with practice exercises, monitor progress, and achieve measurable growth over time. With customizable modules, intuitive dashboards, and real-time feedback, Practice Hub empowers users to build mastery and confidence in any area of study or training."
                 )
                 .fontWeight(.light)
                 .font(.system(.caption))
@@ -47,6 +48,10 @@ struct ContentView: View {
                 .rotationEffect(.degrees(10), anchor: UnitPoint(x: 0, y: 0))
                 .rotation3DEffect(.degrees(25), axis: (x: 1, y: 0, z: 0))
                 .shadow(color: .indigo, radius: 2, x: 0, y: 1)
+                Text("[Link](https://www.google.com) to google.com")
+                    .fontWeight(.semibold)
+                    .font(.footnote)
+                    .foregroundColor(Color(.black))
             }
 
         }.padding()
